9d967964f76c885530ecb39ee0a953db9c23e94d
goodkele <goodkele@gmail.com>
2018-06-14 01:09:46 +0800

    常规更新

--- php/yafexample/application/modules/opacity/boot.php
+++ php/yafexample/application/modules/opacity/boot.php
@@ -24,6 +24,9 @@ $config = new \Yaf\Config\Ini(APP_PATH . "/application/modules/opacity/conf/appl
 \Yaf\Registry::get(\GameConst::CURRENTVIEW)->assign("HTTP_STATIC_URL", $config->http->static_url);
 \Yaf\Registry::get(\GameConst::CURRENTVIEW)->assign("HTTP_HOST_URL", $config->http->host_url);
 
+\Yaf\Registry::get(\GameConst::CURRENTVIEW)->assign("VIEW_PATH", $config->view_path);
+
+
 
 
 
