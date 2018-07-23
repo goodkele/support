f7181d27e1b4a7e2432d559e9ce6f0f367c1f146
DESKTOP-9K8H6EN\user99 <goodkele@gmail.com>
2018-07-23 18:54:43 +0800

    常规更新

--- php/lardemo/database/seeds/FeedsTableSeeder.php
+++ php/lardemo/database/seeds/FeedsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Feed;
 
 class FeedsTableSeeder extends Seeder
 {
@@ -12,10 +13,27 @@ class FeedsTableSeeder extends Seeder
     public function run()
     {
         //
-        DB::table('feeds')->insert([
-            'user_id' => 1,
-            'feed_content' => str_random(10),
-            'feed_mark' => time(),
-        ]);
+        // DB::table('feeds')->insert([
+        //     'user_id' => 1,
+        //     'feed_content' => str_random(10),
+        //     'feed_mark' => time(),
+        // ]);
+
+        factory(\App\Feed::class, 10)->create()->each(function($feed) {
+            var_dump($feed);
+        });
+        
+        exit();
+
+        // factory(\App\Feed::class, 10)->create()->each(function ($a, $b) {
+
+
+        //     //var_dump($feed);
+        //     //$feed->save();
+
+
+        //     //$u->posts()->save(factory(App\Post::class)->make());
+        // });
+
     }
 }
