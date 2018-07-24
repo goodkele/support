69d8c1b695346f086bb9c13be9dd292c5ef2de88
DESKTOP-9K8H6EN\user99 <goodkele@gmail.com>
2018-07-24 16:12:08 +0800

    常规更新

--- php/lardemo/app/Feed.php
+++ php/lardemo/app/Feed.php
@@ -10,14 +10,16 @@ class Feed extends Model
 {
     // use Notifiable;
 
-    // /**
-    //  * The attributes that are mass assignable.
-    //  *
-    //  * @var array
-    //  */
-    // protected $fillable = [
-    //     'name', 'email', 'password',
-    // ];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id', 'feed_content', 'feed_mark',
+    ];
+
+    
 
     // /**
     //  * The attributes that should be hidden for arrays.
--- php/lardemo/database/seeds/FeedsTableSeeder.php
+++ php/lardemo/database/seeds/FeedsTableSeeder.php
@@ -12,18 +12,41 @@ class FeedsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
-        // DB::table('feeds')->insert([
-        //     'user_id' => 1,
-        //     'feed_content' => str_random(10),
-        //     'feed_mark' => time(),
-        // ]);
-
-        factory(\App\Feed::class, 10)->create()->each(function($feed) {
-            var_dump($feed);
-        });
         
-        exit();
+        // 10000 
+
+        
+
+
+        for ($i=0; $i<10000000; $i++) {
+            $mtime = microtime();
+            $mtime = explode(" ", $mtime);
+            $mtime = $mtime[1] . "" . ($mtime[0] * 100000000);
+
+            $feed_content = str_random(mt_rand(500, 5000));
+
+            DB::table('feeds')->insert([
+                'user_id' => mt_rand(1, 100000),
+                'feed_content' => $feed_content, //str_random(10),
+                'feed_from' => mt_rand(1, 5),
+                'like_count' => mt_rand(1,1000),
+                'feed_view_count' => mt_rand(1,1000),
+                'feed_comment_count' => mt_rand(1,1000),
+                //'feed_client_id' => ''
+                'feed_mark' => $mtime, //md5($feed_content), //time(),
+                'created_at' => date("Y-m-d H:i:s"),
+            ]);
+        }
+
+
+        // factory(\App\Feed::class, 10)->create()->each(function($feed) {
+        //     var_dump($feed);
+        // });
+
+        // $feeds = factory(App\Feed::class, 1)->make();
+        // var_dump($feeds);
+        
+        //exit();
 
         // factory(\App\Feed::class, 10)->create()->each(function ($a, $b) {
 
