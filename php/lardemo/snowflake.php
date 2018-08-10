29981ed11c46a0ceb9c5cc43d03dc7eb8d705343
DESKTOP-9K8H6EN\user99 <goodkele@gmail.com>
2018-08-10 17:32:18 +0800

    常规更新

--- php/lardemo/snowflake.php
+++ php/lardemo/snowflake.php
@@ -5,6 +5,18 @@ namespace Goodkele\Support\Snowflake;
 /**
  * Twitter Snowflake算法 生成全局唯一ID
  * 
+ * 1 2 ~ 41 42 ~ 52 53 ~ 64
+ * ID 生成策略
+ * 毫秒级时间41位+机器ID 10位+毫秒内序列12位。
+ * 0           41     51     64
+ * +-----------+------+------+
+ * |time       |pc    |inc   |
+ * +-----------+------+------+
+ *  前41bits是以微秒为单位的timestamp。
+ *  接着10bits是事先配置好的机器ID。
+ *  最后12bits是累加计数器。
+ *  macheine id(10bits)标明最多只能有1024台机器同时产生ID，sequence number(12bits)也标明1台机器1ms中最多产生4096个ID，
+ * 
  * @author wulei
  */
 class Snowflake 
@@ -82,6 +94,8 @@ class Snowflake
         return intval(microtime(true) * 1000) - self::BEGIN_TIME;
     }
 
+    
+
     /**
      * 生成GUID
      */
@@ -120,6 +134,7 @@ class Snowflake
     }
 
 }
+
 
 
 $s = new Snowflake();
